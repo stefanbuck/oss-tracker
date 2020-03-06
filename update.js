bec773d588bf4c40824b7d6a5a28255ef9b639a0
Stefan Buck <stefanb@brandwatch.com>
2020-03-06 23:56:09 +0100

    Store time log in repo

--- update.js
+++ update.js
@@ -2,9 +2,8 @@
 
 // Update data.json
 
-const fs = require('fs');
-const data = require('./data.json')
-const html = fs.readFileSync('./template.html', 'utf8');
+const fs = require("fs");
+const data = require("./data.json");
 const githubEvent = require(process.env.GITHUB_EVENT_PATH);
 
 const { id, body, created_at, updated_at } = githubEvent.comment;
@@ -15,14 +14,4 @@ data[id] = {
     updated_at,
 }
 
-// Update index.html
-
-const table = Object.entries(data).map(([id, data]) => {
-    return `<div>${id} - ${data.body.split(/[\n\r]/)[0]}</div>`;
-}).join('\n')
-
-const finalHTML = eval(`\`${html}\``);
-
-// Write files to disk
 fs.writeFileSync('./data.json', JSON.stringify(data, null, ' '));
-fs.writeFileSync('./public/index.html', finalHTML);
